a405b8ea05942c2449e95f2db8c00e87aa40126d
Chris Jeon <cjeon97@gmail.com>
2019-01-28 18:22:50 -0500

    Added a title, descriptors, and proper axis labels

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ data = pd.read_csv('mtcars.tsv', sep='\t', skiprows=4)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+colors = {
+    'background': '#ffffff',
+    'text': '#000000'
+}
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 
@@ -19,9 +24,26 @@ for i in range(0, 10):
 
 
 app.layout = html.Div([
+    html.H1(
+        children='Motor Trend Car Road Tests',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(
+        children='Comparing Car Models on MPG, CYL, DISP, HP, and DRAT',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+
     html.Div([
         dcc.Dropdown(
-            id="model-column",
+            id="variable-column",
             options=[{'label': i, 'value': i} for i in stats],
             value='mpg'
         ),
@@ -33,15 +55,15 @@ app.layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output('model-graphic', 'figure'),
-    [dash.dependencies.Input('model-column', 'value')]
+    [dash.dependencies.Input('variable-column', 'value')]
      
 )
-def update_model(model_column):
+def update_model(variable_column):
     
     return {
         'data': [go.Scatter(
             x= models,
-            y= data[model_column],
+            y=data[variable_column],
             mode='markers',
             marker={
                 'size': 15,
@@ -52,11 +74,11 @@ def update_model(model_column):
 
         'layout': go.Layout(
             xaxis={
-                'title': model_column
+                'title': 'Car Models'
                 
             },
             yaxis={
-                'title': 'stat'
+                'title': variable_column
                 
             },
             margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
