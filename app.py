af9a56b7a4e8b0b1c5817d4b0c0da2994625142a
Chris Jeon <cjeon97@gmail.com>
2019-01-28 16:56:19 -0500

    Create dictionaries for mpg, cyl, and disp

--- app.py
+++ app.py
@@ -10,10 +10,31 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-models =[]
 
-for i in data.index:
+models = []
+mpgvalues =[]
+cylvalues = []
+dispvalues = []
+stats = ['mpg', 'cyl', 'disp' ]
+
+for i in range(0, 10):
     models.append(data['model'][i])
+
+for i in range(0, 10):
+    mpgvalues.append(data.iloc[i][1])
+  
+
+for i in range(0, 10):
+    cylvalues.append(data.iloc[i][2])
+    
+
+for i in range(0, 10):
+    dispvalues.append(data.iloc[i][3])
+
+mpg_dict = dict(zip(models, mpgvalues))
+cyl_dict = dict(zip(models, cylvalues))
+disp_dict = dict(zip(models, dispvalues))
+
 
 
 app.layout = html.Div([
@@ -22,17 +43,35 @@ app.layout = html.Div([
             id="model-column",
             options=[{'label': i, 'value': i} for i in models],
             value='Mazda RX4'
-
-        )
-
-
-
-
+        ),
+        dcc.Dropdown(
+            id='stat-column',
+            options=[
+                {'label': 'MPG', 'value': stats[0]},
+                {'label': 'CYL', 'value': stats[1]},
+                {'label': 'DISP', 'value': stats[2]}
+            ],
+            value='MPG'
+        ),
+        dcc.Graph(id='model-graphic')
     ])
-
-
-
 ])
+
+
+@app.callback(
+    dash.dependencies.Output('model-graphic', 'figure'),
+    [dash.dependencies.Input('stat-column', 'value'),
+     dash.dependencies.Input('model-column', 'value')]
+)
+def update_model(model_column):
+
+    return {
+        'data': [go.Scatter(
+            
+        )]
+
+
+    }
 
 
 if __name__ == '__main__':
